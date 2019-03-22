c1dd3c88233e2c78dff2ce249c555bec449903df
naomikeusch <naomikeusch@users.noreply.github.com>
2019-03-22 18:23:53 -0400

    Create Hw4.java

---
+++ Hw4.java
@@ -0,0 +1,9 @@
+//Hello
+/*
+THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING A TUTOR OR CODE
+WRITTEN BY OTHER STUDENTS - NAOMI KEUSCH BAKER AND MARTIN SCHREINER
+*/
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import weka.core.converters.ConverterUtils.DataSource;
