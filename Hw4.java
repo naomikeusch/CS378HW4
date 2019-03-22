4adbf0c21cc7f66c84c98691818e7ba2cf20bc79
naomikeusch <naomikeusch@users.noreply.github.com>
2019-03-22 18:26:23 -0400

    Update Hw4.java

--- Hw4.java
+++ Hw4.java
@@ -7,3 +7,14 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 import weka.core.converters.ConverterUtils.DataSource;
+
+ import weka.core.Instances;
+ import weka.experiment.InstanceQuery;
+
+ InstanceQuery query = new InstanceQuery();
+ query.setUsername("nobody");
+ query.setPassword("");
+ query.setQuery("select * from whatsoever");
+ // You can declare that your data set is sparse
+ // query.setSparseData(true);
+ Instances data = query.retrieveInstances();
